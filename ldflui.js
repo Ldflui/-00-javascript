7a9babe79cab353e85d43e4cc023ab573499214f
Luis Najarro <luisnajarrito11@gmail.com>
2024-06-23 02:02:11 -0500

    Guardando mi proyecto 00-javascript

---
+++ ldflui.js
@@ -0,0 +1,13 @@
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//comentario en una sola linea
+
+/* comentario
+en multiples
+lineas*/
+
+let num = 1
+const pi = 3.1416
+
+let integer = 1; 2; 3
+let float = 1.1; 1.2
